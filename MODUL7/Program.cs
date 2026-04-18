d781ff65d0c917d2e1920adbfe9e6aee6f73ada4
josefhint755-netizen <josefhint755@gmail.com>
2026-04-18 15:05:10 +0700

    Menambahkan Class Watchlist_103022400045

--- MODUL7/Program.cs
+++ MODUL7/Program.cs
@@ -1,3 +1,4 @@
 ﻿using MODUL7;
 
 FilmFavorit_103022400045.ReadJSON();
+Watchlist_103022400045.ReadJSON();
---
+++ MODUL7/Watchlist_103022400045.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+public class Movie
+{
+    public string id { get; set; }
+    public string title { get; set; }
+    public int year { get; set; }
+    public string genre { get; set; }
+    public double rating { get; set; }
+}
+
+public class WatchlistData
+{
+    public string watchlistName { get; set; }
+    public string createdBy { get; set; }
+    public List<Movie> movies { get; set; }
+}
+
+public class Watchlist_103022400045
+{
+    public static void ReadJSON()
+    {
+        string json = File.ReadAllText("jurnal7_2_103022400045.json");
+        WatchlistData data = JsonSerializer.Deserialize<WatchlistData>(json);
+
+        Console.WriteLine("Watchlist Name : " + data.watchlistName);
+        Console.WriteLine("Created By : " + data.createdBy);
+        Console.WriteLine("Movies : ");
+        foreach (var m in data.movies)
+        {
+            Console.WriteLine($"{m.id} {m.title} ({m.year} - {m.rating})");
+        }
+    }
+}
