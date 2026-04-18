da8e2defc3049bcdae5d15079b90e7a49792f727
fiqriananda <fiqriananandahakin@gmail.com>
2026-04-18 15:15:18 +0700

    feat:add genredictanoary

--- MODUL7/Program.cs
+++ MODUL7/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MODUL7;
 
-Watchlist_103022430003 wl = new Watchlist_103022430003();
-wl.ReadJSON();
+GenreDictionary_103022430003 gd = new GenreDictionary_103022430003();
+
+gd.ReadJSON();
