639a799f5072d8ea1235e5d39e5674f5a520d82d
fiqriananda <fiqriananandahakin@gmail.com>
2026-04-18 14:25:39 +0700

    Feat: Add FilmFavorite_103022430003 Class

--- MODUL7/Program.cs
+++ MODUL7/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using MODUL7;
+
+FilmFavorite_103022430003 ff = new FilmFavorite_103022430003();
+
+ff.ReadJSON();
