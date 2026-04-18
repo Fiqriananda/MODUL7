e4c0d3436a5ce508accc999da038e00182909bd7
FairuzHamzahF4izal <fairuzhamzah1@gmail.com>
2026-04-18 15:07:37 +0700

    menambahkan class WatchList_103022400091_PRAKTIKAN

--- MODUL7/Program.cs
+++ MODUL7/Program.cs
@@ -3,5 +3,7 @@
     static void Main(string[] args)
     {
         FilmFavorit_103022400091_PRAKTIKAN.ReadJSON();
+        Console.WriteLine();
+        WatchList_103022400091_PRAKTIKAN.ReadJSON();
     }
 }
---
+++ MODUL7/WatchList_103022400091_PRAKTIKAN.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+
+public class WatchList_103022400091_PRAKTIKAN
+{
+    public string WatchListName { get; set; }
+
+    public string CreatedBy  { get; set; }
+
+    public List<Movie> movies {  get; set; }
+
+    public class Movie
+    { 
+        public string id { get; set; } 
+        public string title { get; set; } 
+        public int year { get; set; }
+        public string genre { get; set; }
+        public double rating { get; set; }
+    }
+
+    public static void ReadJSON()
+    {
+        string json = File.ReadAllText("jurnal7_2_103022400091.json");
+        var data = JsonSerializer.Deserialize<WatchList_103022400091_PRAKTIKAN>(json);
+
+        Console.WriteLine($"Watch Listnya : {data.WatchListName}");
+        Console.WriteLine($"Dibuat Oleh : {data.CreatedBy}");
+
+        int i = 1;
+        foreach ( var item in data.movies)
+        {
+            Console.WriteLine($"List Film {i} {item.id} - {item.title} - {item.year} - {item.genre} - {item.rating}");
+            i++;
+        }
+        
+    }
+}
